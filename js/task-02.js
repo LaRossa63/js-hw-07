88c5b1ee7fdd7400e30a2bc42b0c85e2fd890538
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 11:42:28 +0400

    Задание 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,22 @@
-console.log(2);
+const ref = {
+  output: document.getElementById('ingredients'),
+};
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const r = ingredients.map((ingredient) => {
+  const li = document.createElement('li');
+
+  li.textContent = ingredient;
+
+  return li;
+});
+
+ref.output.prepend(...r);
