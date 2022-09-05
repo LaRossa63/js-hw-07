878013478f269be308825f4f01395417f51e7bb1
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 14:48:37 +0400

    Задание 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,12 @@
-console.log(7);
+const ref = {
+  input: document.getElementById('font-size-control'),
+  output: document.getElementById('text'),
+};
+
+const handleChangeSlider = (event) => {
+  const value = event.target.value;
+
+  ref.output.style.fontSize = `${value}px`;
+};
+
+ref.input.addEventListener('input', handleChangeSlider);
