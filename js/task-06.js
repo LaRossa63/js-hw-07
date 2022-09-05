d07d77aac6ee0d70798909491bb2e05eb9130d82
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 13:51:40 +0400

    Задание 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,24 @@
-console.log(6);
+const ref = {
+  input: document.querySelector('.validation__input'),
+  maxLength: Number(
+    document.querySelector('.validation__input').dataset.length
+  ),
+};
+
+const handleInputBlur = (event) => {
+  const value = event.target.value;
+
+  if (value.length === ref.maxLength) {
+    ref.input.classList.add('valid');
+    return;
+  }
+
+  ref.input.classList.add('invalid');
+};
+
+const handleInputFocus = () => {
+  ref.input.className = 'validation__input';
+};
+
+ref.input.addEventListener('blur', handleInputBlur);
+ref.input.addEventListener('focus', handleInputFocus);
