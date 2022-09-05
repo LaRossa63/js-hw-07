f118251b9f3ff8c930445eedaad8fc20512c3d01
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 11:36:33 +0400

    Исправление, сделал Задание 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,11 @@
-console.log(1);
+const ref = {
+  item: document.querySelectorAll('.item'),
+};
+
+ref.item.forEach((element) => {
+  console.log('');
+  console.log(`Категория: ${element.firstElementChild.textContent}`);
+  console.log(
+    `Количество элементов: ${element.querySelector('ul').childElementCount}`
+  );
+});
