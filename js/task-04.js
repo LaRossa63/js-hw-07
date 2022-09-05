07f6c4f2de6321970029a54a13fdf546e865f498
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 12:26:40 +0400

    Задание 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,25 @@
-console.log(4);
+const ref = {
+  decrement: document.querySelector('[data-action="decrement"]'),
+  increment: document.querySelector('[data-action="increment"]'),
+
+  output: document.getElementById('value'),
+};
+
+const getCurrentValue = () => {
+  return Number(ref.output.textContent);
+};
+
+const setValue = (value) => {
+  ref.output.textContent = value;
+};
+
+const handleClickDecrement = () => {
+  setValue(getCurrentValue() - 1);
+};
+
+const handleClickIncrement = () => {
+  setValue(getCurrentValue() + 1);
+};
+
+ref.decrement.addEventListener('click', handleClickDecrement);
+ref.increment.addEventListener('click', handleClickIncrement);
