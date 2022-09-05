3f2721d84a0cbbe32a1bfcc36b6469c57eed7a2d
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 13:42:38 +0400

    Задание 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,17 @@
-console.log(5);
+const ref = {
+  input: document.getElementById('name-input'),
+  output: document.getElementById('name-output'),
+};
+
+const handleChangeInput = (event) => {
+  const value = event.target.value;
+
+  if (!value.length) {
+    ref.output.textContent = 'незнакомец';
+    return;
+  }
+
+  ref.output.textContent = value;
+};
+
+ref.input.addEventListener('input', handleChangeInput);
