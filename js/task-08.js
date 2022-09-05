5ec4f43b032a5ff0ec8c0df29797abe13cbb27e4
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 15:17:37 +0400

    Задание 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,64 @@
-console.log(8);
+const ref = {
+  input: document.querySelector('.controls__input'),
+
+  create: document.querySelector('[data-action="render"]'),
+  clear: document.querySelector('[data-action="destroy"]'),
+
+  output: document.querySelector('.boxes'),
+};
+
+const getInputValue = () => {
+  return Number(ref.input.value);
+};
+
+const getBoxSize = () => {
+  const numberElements = document.querySelectorAll('.boxes__box').length;
+  const start = 30;
+  const step = 10;
+
+  if (!numberElements) {
+    return start;
+  }
+
+  return numberElements * step + start;
+};
+
+function getRBG() {
+  var o = Math.round,
+    r = Math.random,
+    s = 255;
+  return (
+    'rgba(' +
+    o(r() * s) +
+    ',' +
+    o(r() * s) +
+    ',' +
+    o(r() * s) +
+    ',' +
+    r().toFixed(1) +
+    ')'
+  );
+}
+
+const createBoxes = (count) => {
+  for (let i = 0; i < count; i++) {
+    const size = getBoxSize();
+
+    ref.output.insertAdjacentHTML(
+      'beforeend',
+      `<div style="width: ${size}px; height: ${size}px; background-color: ${getRBG()};" class="boxes__box"> </div>`
+    );
+  }
+};
+
+const handleClickCreate = () => {
+  createBoxes(getInputValue());
+};
+
+const handleClickClear = () => {
+  ref.output.innerHTML = '';
+  ref.input.value = '';
+};
+
+ref.create.addEventListener('click', handleClickCreate);
+ref.clear.addEventListener('click', handleClickClear);
